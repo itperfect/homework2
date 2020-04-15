458bea0490c62c16350103345fb5738b39c4ce7b
Artyom Salkov <artyom.salkov@gmail.com>
2020-04-15 23:36:10 +0300

    task2-1 ready

--- task2-1.py
+++ task2-1.py
@@ -1,1 +1,19 @@
-print("Homework #2 task #1")
+# 1. Написать функцию, которая будет принимать на вход натуральное число n, и возращать сумму его цифр.
+# Реализовать используя рекурсию (без циклов, без строк, без контейнерных типов данных).
+# Пример: get_sum_of_components(123) -> 6 (1+2+3)
+
+def get_sum_of_components(summ = 0):
+    if summ < 1:
+        return 0
+    else:
+        num = summ / 10
+        ost = round(num%1, 1) * 10
+
+        num = int(num)
+        ost = int(ost)
+
+        return ost + get_sum_of_components(num)
+
+
+res = get_sum_of_components(135 + 531)
+print(res)
