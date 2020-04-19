859bc5f8a72dbd1b2b8d30c513b139a5b681ab5a
Artyom Salkov <artyom.salkov@gmail.com>
2020-04-19 15:53:10 +0300

    task2-2 ready

--- task2-2.py
+++ task2-2.py
@@ -1,1 +1,26 @@
-print("Homework #2 task #2")
+# 2. Написать декоратор log, который будет выводить на экран все аргументы, которые передаются вызываемой функции.
+# @log
+# def my_sum(*args):
+# return sum(*args)
+
+# my_sum(1,2,3,1) - выведет "Функция была вызвана с - 1, 2, 3, 1"
+# my_sum(22, 1) - выведет "Функция была вызвана с - 22, 1"
+
+
+def log(func):
+
+    def wrapper(*args):
+        args_in_string = ", ".join(map(str, args))
+        print(f'Функция была вызвана с - {args_in_string}')
+
+    return wrapper
+
+
+@log
+def my_sum(*args):
+    pass
+
+
+if __name__ == "__main__":
+    my_sum(1,2,3,1)
+    my_sum(22,1)
